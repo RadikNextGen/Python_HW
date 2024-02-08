bb7e0cf5595cccdf2dc420062f025b3dd38350ea
RadikNextGen <146774298+RadikNextGen@users.noreply.github.com>
2024-02-08 21:57:27 +0300

    Add files via upload

---
+++ Tree Path/tree_path.py
@@ -0,0 +1,66 @@
+def find_path(node, target_sum, current_sum, path, tree, q):
+    if node is None:
+        return None
+    current_sum += node
+    path[0].append(node)
+    path[1].append(q)
+    if not tree:
+        if current_sum == target_sum:
+            return path
+        else:
+            return None
+    for child in range(len(tree[0])):
+        if ((child - q) == 1 or (child - q) == 2 and child) or ((child - q) == 0 or (child - q) == 1 and not(child)):
+            found_path = find_path(tree[0][child], target_sum, current_sum, [path[0][:], path[1][:]], tree[1:], child)
+            if found_path:
+                return found_path
+    return None
+
+
+
+def tree_illustrator(tree, depth, path):
+    for i in range(depth):
+        n = len(tree[i])
+        spaces_between = 2 ** (depth - i + 1) - 1
+        spaces_around = 2 ** (depth - i) - 1
+        print(' ' * spaces_around, end='')
+        for j in range(n):
+            if tree[i][j] is not None:
+                print(tree[i][j], end='')
+                if path:
+                    if path[1][i] == j:
+                        print('*', end='')
+            else:
+                print('', end='')
+            if j < n - 1:
+                print(' ' * spaces_between, end='')
+        print()
+
+
+print("Введите целевую сумму:")
+target_sum = int(input())
+tree = []
+print("Введите строку, описывающую древо:")
+a = [int(x) if x else None for x in input().split(',')]
+n = len(a)
+i = 0
+while i < n:
+    row = a[i:2*i+1]
+    tree.append(row)
+    i = 2*i+1
+depth = len(tree)
+
+path = find_path(tree[0][0], target_sum, 0, [[],[]], tree[1:], 0)
+if path:
+    print("Найден путь:")
+    n = len(path[0])
+    for i in range(n):
+        if n - i - 1:
+            print(path[0][i], "->", end=' ')
+        else:
+            print(path[0][i])
+else:
+    print("Путь не найден.")
+
+print("\n","preAlpha-версия Tree Illustrator 3000")
+tree_illustrator(tree, depth, path)
